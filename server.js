4d500cce3eedfaad9526db5581c54c8fcfaf0e9d
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-10 00:00:05 -0400

    Import sequelize and add comments

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+// Import modules
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require("sequelize");
 
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configure app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Establish route handling based on routes directory
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+// Launch server to begin listening for requests
+// NOTE -- update to force: false before deploying to production
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });

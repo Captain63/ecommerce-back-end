26d91818fdd46e751c74035a11d8e0f1c7327d29
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-11 16:42:13 -0400

    Add aliases to Product and Tag associations

--- models/index.js
+++ models/index.js
@@ -16,14 +16,14 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag
   },
-  as: "product_tags"
+  as: "tags"
 })
 
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag
   },
-  as: "product_tags"
+  as: "products"
 })
 
 module.exports = {
